ccf6695189a32325300df2a7e1c9f9c4a7bd4906
panosalbanis <panos.albanis@mailonline.co.uk>
2014-07-18 16:30:00 +0100

    Fixed compilation issue

--- src/s_metric/combined.clj
+++ src/s_metric/combined.clj
@@ -1,6 +1,7 @@
 (ns s-metric.combined
   (:require [s-metric.hamming :as hamming]
             [s-metric.naive-match :as naive]
+            [clojure.set :as s]
             [clojure.core.reducers :as r]))
 
 (defn match [s target]
@@ -26,14 +27,14 @@
     (let [top (conj topn score)
           lowest (first top)]
       (if (> (count top) n)
-        (clojure.set/difference top #{lowest})
+        (s/difference top #{lowest})
         top))))
 
 (defn combinef [n] 
   (fn [top1 top2]
-    (let [top (clojure.set/union top1 top2)
+    (let [top (s/union top1 top2)
           lowest (take n top)]
-      (clojure.set/difference top (into (sorted-set) lowest)))))
+      (s/difference top (into (sorted-set) lowest)))))
 
 (defn top-scores 
   "Applies match-% to a collection of targets returning the top <= n highest scores as
